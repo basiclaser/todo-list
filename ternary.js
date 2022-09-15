49902c1b06a5772d8f3ca2301cfb6f6534bd061c
Christopher Shelley <dont@forgetmy.email>
2022-09-15 18:08:14 +0200

    add filter, toggle, inline editor

---
+++ ternary.js
@@ -0,0 +1,12 @@
+// WHAT IS A TERNARY 
+
+const isNiceWeather = true
+
+let a;
+if (isNiceWeather) {
+    a =  "ok lets go out"
+} else {
+    a = "no sorry i cant"
+}
+
+let b = isNiceWeather ? "ok" : "no sorry i cant"
--- todo.js
+++ todo.js
@@ -10,72 +10,101 @@ console.log("yep you connected js ")
 // adding / checking / updating / deleting things in an array. (CRUD)
 //                0        1         2
 const myList = [
-    {done: false, text:"apple"},
-    {done: false, text:"banana"},
-    {done: false, text:"coffee"}
+    {pending: true, text:"apple", date: new Date()},
+    {pending: true, text:"banana", date: new Date()},
+    {pending: true, text:"coffee", date: new Date()}
 ]
+let editingIndex = 2
+
 const inputEl = document.querySelector("#user-text-input-element")
 const targetEl = document.querySelector("#target")
+const pendingToggleEl = document.querySelector("#pending-toggle")
 
-
-
-// LISTEN / PAY ATTENTION / OBSERVER / WAIT 
-// DOM NODE 
-// addEventListener(event type, callback function to handle the event) <-- signature
+// CREATE
 inputEl.addEventListener("keyup", function(e){
     console.log(e)
     if(e.key === "Enter") {
         console.log(inputEl.value)
-        myList.unshift(inputEl.value)
+
+        const newItem = {
+            pending:true,
+            text:inputEl.value,
+            date: new Date()
+        }
+
+        myList.unshift(newItem)
         render()
     }
 })
-
-// inputEl.addEventListener("keydown", (e)=>console.log(e.type))
-// inputEl.addEventListener("keypress", (e)=>console.log(e.type))
-// inputEl.addEventListener("keyup", (e)=>console.log(e.type))
-
-// CREATE
-// unshift
-// myList.unshift("spaghetti")
-
-
-// READ
-// create HTML, adding to the DOM (the page)
-
+// READ 
 function render() {
-    // take the data ( myList )
-    // turn into "html" strings
-    // put it into the dom 
-    // "PRINT"
     const myListAsHTML = myList
-        .map((item, i) => `
-        <div>
-            ${item.text}
-            <!--put the toggle done/not done button here-->
-            <button onclick="remove(${i})">🗑</button>
-        </div>`)
-        // .reverse()
-        // .sort()
+        // "if showOnly pending, allow only pending:true items through the filter"
+        .filter(function(item){
+            return showOnlyPending ? item.pending : true
+        })
+        .map(function(item, i) {
+            return `
+                <div>
+                    ${item.date.toLocaleDateString('en-US', {
+                        year: 'numeric',
+                        month: '2-digit',
+                        day: '2-digit',
+                      })}
+                    ${editingIndex === i ? `<input onkeyup="handleInlineEditorKeyup(event, ${i})" type="text" value="${item.text}"></input>` : `<span ondblclick="setMeAsActivelyBeingEdited(${i})" >${item.text}</span>`}
+                    ${item.pending}
+                    <!--put the toggle pending/not pending button here-->
+                    <button onclick="toggleDone(${i})">✅</button>
+                    <button onclick="remove(${i})">🗑</button>
+                </div>`
+        })
         .join("")
-
-    console.log(myListAsHTML)
     targetEl.innerHTML = myListAsHTML
 }
-
-
 // UPDATE
-// myList[userClickIndex] = inputField.value
-
-
-// DELETE
-// myList.splice(i,1)
-
-function remove(bazinga) {
-    myList.splice(bazinga,1)
+function toggleDone(i) {
+    editingIndex = i
+    myList[i].pending = !myList[i].pending
     render()
 }
 
+function setMeAsActivelyBeingEdited(i) {
+    editingIndex = i
+    render()
+} 
+function handleInlineEditorKeyup(event, i) {
+    console.log("hi", i, event)
+    if(event.key === "Escape") {
+        editingIndex = -1
+        render()
+    }
+    if(event.key === "Enter") {
+        myList[i].text = event.target.value
+        editingIndex = -1
+        render()
+    }
+}
+
+function toggleDone(i) {
+    myList[i].pending = !myList[i].pending
+    render()
+}
+
+// DELETE
+function remove(i) {
+    myList.splice(i,1)
+    render()
+}
+
+// FILTER OUT DONE
+// A NEW PIECE OF STATE IN OUR APP 
+let showOnlyPending = false;
+pendingToggleEl.addEventListener("click", function(event){
+    showOnlyPending = !showOnlyPending
+    pendingToggleEl.innerText = showOnlyPending ? "remove filter" : "show only pending"
+    render()
+    console.log(showOnlyPending)
+})
 
 // "initial render" // "initial draw"
 render()
